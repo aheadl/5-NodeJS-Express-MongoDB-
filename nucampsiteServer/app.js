fa61e3240cf14655f18722b3a9036a6b64feb77d
Andrea <mheadleybrooks@outlook.com>
2021-01-08 14:06:48 -0500

    Express REST API with MongoDB and Mongoose Part 1

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -4,16 +4,26 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+const mongoose = require("mongoose");
+const url = "mongodb://localhost:27017/nucampsite";
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connect.then(
+  () => console.log("Connected correctly to server"),
+  (err) => console.log(err)
+);
+
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-
-const campsiteRouter = require("./routes/campsiteRouter");
-const promotionRouter = require("./routes/promotionRouter");
-const partnerRouter = require("./routes/partnerRouter");
 
 var app = express();
 
