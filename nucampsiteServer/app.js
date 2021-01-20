349e91507302891033e57587a63cfb4c3bb99fed
Andrea <mheadleybrooks@outlook.com>
2021-01-20 17:45:09 -0500

    File Upload

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -30,6 +30,8 @@ const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
 
+const uploadRouter = require("./routes/uploadRouter");
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
@@ -131,6 +133,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
+app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
